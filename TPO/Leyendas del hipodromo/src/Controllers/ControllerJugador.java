cb1aebf8e35c897eed856e7745cdc5a07cfdfae1
Rocio Belen Ianniello <ianniellorb@gmail.com>
2026-05-25 02:29:06 -0300

    Fix modificacion de la clase jugador y del controller

--- TPO/Leyendas del hipodromo/src/Controllers/ControllerJugador.java
+++ TPO/Leyendas del hipodromo/src/Controllers/ControllerJugador.java
@@ -1,4 +1,11 @@
 package Controllers;
 
+import Models.Jugador;
+
 public class ControllerJugador {
+    private Jugador jugador;
+
+    public Jugador CrearJugador(String nombre, String mail) {
+        return new Jugador(nombre, mail);
+    }
 }
--- TPO/Leyendas del hipodromo/src/Models/Jugador.java
+++ TPO/Leyendas del hipodromo/src/Models/Jugador.java
@@ -7,4 +7,31 @@ public class Jugador{
     private int puntaje;
     private Caballo caballo;
     private int posicionCarrera;
+
+    public Jugador(String nombre, String mail) {
+        this.nombre  = nombre;
+        this.mail    = mail;
+        this.puntaje = 0;
+        this.caballo = null;
+    }
+
+    public void sumarPuntaje(int puntos) {
+        this.puntaje += puntos;
+    }
+
+    public void asignarCaballo(Caballo caballo) {
+        this.caballo = caballo;
+    }
+
+
+    public String getNombre()  { return nombre;  }
+    public String getMail()    { return mail;    }
+    public int    getPuntaje() { return puntaje; }
+    public Caballo getCaballo(){ return caballo; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setMail(String mail)     { this.mail   = mail;   }
 }
+
+
+
