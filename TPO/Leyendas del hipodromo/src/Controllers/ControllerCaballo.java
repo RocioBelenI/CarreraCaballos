c17cbc38691ed5c428740508d6770cce5fb1a5bf
Santiago Cernius <santiagocernius@gmail.com>
2026-06-05 01:48:44 -0300

    Archivo CaballoController

--- TPO/Leyendas del hipodromo/src/Controllers/ControllerCaballo.java
+++ TPO/Leyendas del hipodromo/src/Controllers/ControllerCaballo.java
@@ -1,12 +1,32 @@
 package controllers;
 
+import data.CaballoRepository;
+import data.dao.CaballoDAO;
+import dto.CaballoDTO;
+import java.util.ArrayList;
 import java.util.List;
 
-import models.Carrera;
-import models.Jugador;
-
 public class ControllerCaballo {
+	
+    private CaballoRepository repository = new CaballoRepository();
 
-	
-	
+    public List<CaballoDTO> listarCaballosParaUI() {
+        List<CaballoDTO> listaParaVista = new ArrayList<>();
+
+        List<CaballoDAO> caballosDB = repository.listarCaballos(); 
+
+        
+        for (CaballoDAO c : caballosDB) {
+            listaParaVista.add(new CaballoDTO(
+                c.getId().toString(),
+                c.getNombre(),
+                c.getEmoji(),
+                (double) c.getVelocidad(), 
+                (double) c.getResistencia(),
+                100.0,
+                0.0
+            ));
+        }
+        return listaParaVista;
+    }
 }
